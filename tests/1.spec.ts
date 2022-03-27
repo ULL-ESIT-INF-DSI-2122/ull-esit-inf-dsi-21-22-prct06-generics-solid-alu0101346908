4fb956ef12ae4ebb05c23c42233d5962f8371faf
alu0101346908 <alu0101346908@ull.edu.es>
2022-03-27 12:16:01 +0100

    Se añade el combate entre fighters, multiplicadores de efectividad

--- tests/1.spec.ts
+++ tests/1.spec.ts
@@ -26,11 +26,14 @@ describe('Fighter subclasses tests', () => {
   });
 });
 
-describe('Combat sclass tests', () => {
+describe('Combat class tests', () => {
   const pokemon: Fighter = new Pokemon('Pikachu', 6, 0.4, 'electrico', [55, 40, 90, 35], 'Pi-kaPika');
   // eslint-disable-next-line max-len
   const goku: Fighter = new DragonBall('Son Goku', 62, 1.75, 'saiyan', [9560, 2279, 1500, 26270], 'It\'s over!', 'Kamehameha');
-  const combat1: Combat = new Combat(goku, pokemon);
+  const artoria: Fighter = new FateServant('Artoria Pendragon', 42, 1.54, 'saber', [14418, 7560, 3009, 19506], 'All hands, prepare to charge!', 'Earth');
+  // eslint-disable-next-line max-len
+  const hellagur: Fighter = new ArknightOperator('Helage', 85, 1.93, 'guard', [762, 334, 120, 3825], 'All hands, prepare to charge!', 26);
+  const combat1: Combat = new Combat(goku, artoria);
   it('Debe existir un metodo para iniciar una pelea ', () => {
     expect(combat1.start()).to.exist;
   });

3aa322a5802d7427b15b086568bf14c01f38b1b4
alu0101346908 <alu0101346908@ull.edu.es>
2022-03-27 17:55:55 +0100

    Se añade el cypher a la clase CaesarCypher y pasa la prueba

--- src/ejercicio-3/caesarCypher.ts
+++ src/ejercicio-3/caesarCypher.ts
@@ -5,7 +5,34 @@ export class CaesarCypher extends Cypher {
     super(alphabet, key);
   }
   cypher(text:string): string {
-    return ``;
+    let ampliedkey: string = this.key;
+    let excess: number = 0;
+    while (ampliedkey.length < text.length) {
+      ampliedkey += ampliedkey;
+    }
+    if (ampliedkey.length > text.length) {
+      excess = ampliedkey.length - text.length;
+      ampliedkey = ampliedkey.substring(0, ampliedkey.length-excess);
+    }
+    let index: number = 0;
+    let dummytextchar: string = ``;
+    let dummykeychar: string = ``;
+    let shiftend: number = 0;
+    let shiftstart: number = 0;
+    let encriptedText: string = ``;
+    while (index < text.length) {
+      dummytextchar = text.charAt(index);
+      dummykeychar = ampliedkey.charAt(index);
+      shiftstart = this.alphabet.indexOf(dummytextchar);
+      shiftend = this.alphabet.indexOf(dummykeychar);
+      if (shiftstart == -1) {
+        encriptedText += dummytextchar;
+      } else {
+        encriptedText += this.alphabet.charAt(((shiftstart+shiftend+1)% this.alphabet.length));
+      }
+      index++;
+    }
+    return encriptedText;
   }
   decypher(text:string): string {
     return ``;

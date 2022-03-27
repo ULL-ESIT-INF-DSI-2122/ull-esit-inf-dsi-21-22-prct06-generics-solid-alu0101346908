466595bb46641485209a99450b237fb641d05b59
alu0101346908 <alu0101346908@ull.edu.es>
2022-03-27 12:24:37 +0100

    Se espera una clase Pokedex que almacene figters, permita añadirlos y recuperarlos posteriormente

--- tests/1.spec.ts
+++ tests/1.spec.ts
@@ -38,3 +38,17 @@ describe('Combat class tests', () => {
     expect(combat1.start()).to.exist;
   });
 });
+
+describe('Pokedex class tests', () => {
+  const pokemon: Fighter = new Pokemon('Pikachu', 6, 0.4, 'electrico', [55, 40, 90, 35], 'Pi-kaPika');
+  // eslint-disable-next-line max-len
+  const goku: Fighter = new DragonBall('Son Goku', 62, 1.75, 'saiyan', [9560, 2279, 1500, 26270], 'It\'s over!', 'Kamehameha');
+  const artoria: Fighter = new FateServant('Artoria Pendragon', 42, 1.54, 'saber', [14418, 7560, 3009, 19506], 'All hands, prepare to charge!', 'Earth');
+  const jeanneDArc: Fighter = new FateServant('Jeanne d\'Arc ', 44, 1.59, 'archer', [13523, 5460, 4215, 20270], 'Here we go!', 'Human');
+  // eslint-disable-next-line max-len
+  const hellagur: Fighter = new ArknightOperator('Helage', 85, 1.93, 'guard', [762, 334, 120, 3825], 'All hands, prepare to charge!', 26);
+  const pokedex: Pokedex = new Pokedex([pokemon, goku, artoria, hellagur]);
+  it('Se debe poder añadir un Fighter a la pokedex y obtenerlo posteriormente con un id', () => {
+    expect(pokedex.addFighter(jeanneDArc).getFighter(4)).to.deep.equal(jeanneDArc);
+  });
+});
